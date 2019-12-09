bec0e906b096c4262aaeb99e4a9a2ad4bae05555
Roberto Groza <robertogroza@gmail.com>
2019-12-09 17:58:18 -0500

    added runBlue/RedAutonomous for testing both auto programs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mode/RedBlocksAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mode/RedBlocksAutonomous.java
@@ -2,8 +2,8 @@ package org.firstinspires.ftc.teamcode.mode;
 
 import org.firstinspires.ftc.teamcode.robot.CompetitionBot;
 
-@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="RunAutoTest", group="Test")
-public class RunAutonomousTest extends AutonomousNew {
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="RedBlocksAuto", group="Autonomous")
+public class RedBlocksAutonomous extends AutonomousNew {
     @Override
     public void runOpMode() throws InterruptedException {
         robot = new CompetitionBot(hardwareMap, telemetry);
@@ -13,7 +13,7 @@ public class RunAutonomousTest extends AutonomousNew {
         telemetry.update();
 
         waitForStart();
-        runAutonomous(telemetry);
+        runRedAutonomous(telemetry);
 
     }
 }
