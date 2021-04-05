89a6b2fad68d88f1f3e5571ce6572c25ee9be95b
Roberto Groza <robertogroza@gmail.com>
2021-04-05 14:57:44 -0400

    fixed error

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/RingsVisionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/RingsVisionTest.java
@@ -3,11 +3,17 @@ package org.firstinspires.ftc.teamcode.vision;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.robot.CompetitionBot;
+import org.opencv.core.Point;
+
 @Autonomous(name="RingsVisionTest", group="Test")
 public class RingsVisionTest extends LinearOpMode {
 
     public void runOpMode() {
-        RingsOpenCV vision = new RingsOpenCV(true, hardwareMap, telemetry);
+        CompetitionBot robot = new CompetitionBot(hardwareMap, telemetry);
+
+        Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(robot.STREAM_WIDTH - robot.REGION_WIDTH, (robot.STREAM_HEIGHT - robot.REGION_HEIGHT) / 2 - 70);
+        RingsOpenCV vision = new RingsOpenCV(REGION1_TOPLEFT_ANCHOR_POINT, hardwareMap, telemetry);
 
         waitForStart();
 
